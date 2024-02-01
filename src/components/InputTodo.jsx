1c05c5aa03e70f087c305076148339b95f471196
baqar-abbas <baqar007@hotmail.com>
2024-02-01 14:30:04 +0500

    create todo app states and passed it to other components
    Add logic for adding, deleting to do list tasks

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,41 @@
 //InputTodo component
+/* eslint-disable */
+import { useState } from "react"
+const InputTodo = ({ addTodoItem }) => {
+  const [title,setTitle] = useState('');
+  const [message, setMessage] = useState('');
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  }
 
-const InputTodo = () => {
+const handleSubmit = (e) => {
+e.preventDefault();
+if (title.trim()) {
+  addTodoItem(title);
+  setTitle('');
+  setMessage('');
+} else {
+  setMessage('Please add item.');
+  setTimeout(() => {
+    setMessage('');
+
+  }, 3000)
+}
+}
+
   return (
-    <div>
-        input field here...
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+        <input
+         type='text'
+         value={title}
+         onChange={handleChange} 
+         placeholder='Add todo...' 
+         />
+        <button>Add</button>
+    </form>
+    <span>{message}</span>
+    </>
   )
 }
 
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -2,11 +2,11 @@
 /* eslint-disable */
 import TodoItem from './TodoItem'
 
-const TodosList = ({todos}) => {
+const TodosList = ({todos, setTodos, delTodo}) => {
   return (
     <ul>
     {todos.map((todo) => (
-    <TodoItem itemProp={todo} />
+    <TodoItem itemProp={todo} key={todo.id} setTodos={setTodos} delTodo={delTodo} />
     ))}
   </ul>
   )
