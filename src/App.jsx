45dabbd707dd1035c1baa793addc2c3c15cb315d
baqar-abbas <baqar007@hotmail.com>
2024-05-20 05:13:20 +0500

    Setup routing, nested routing and dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+// import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
+import About from './routes/About'
+import Login from './routes/Login'
+import Profile from './routes/Profile'
+import NotMatch from './routes/NotMatch'
+import SinglePage from './routes/SinglePage'
+import Layout from './components/Layout'
 import './App.css'
-import Header from './components/Header'
-import TodosLogic from './components/TodosLogic'
 
 function App() {
 
   return (
-    <React.Fragment>
-      <div className="wrapper">
-      <div className="todos">
-    <Header />
-    <TodosLogic />
-    </div>
-    </div>
-    </React.Fragment>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} >
+      <Route path=":slug" element={<SinglePage />} />
+      </Route>
+      <Route path='login' element={<Login />} />
+      <Route path='profile' element={<Profile />} />
+      <Route path='*' element={<NotMatch />} />
+      </Route>
+    </Routes>
   )
 }
 
